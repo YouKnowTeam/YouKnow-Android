94a099b88a3b4368f70ca5422dc1b4dfdfc02476
Liumeo <liumeo@pku.edu.cn>
2018-05-14 19:23:59 +0800

    main activity

---
+++ app/src/main/java/com/liumeo/jizhi/MainActivity.java
@@ -0,0 +1,45 @@
+package com.liumeo.jizhi;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity
+{
+	View confirmTextView;
+	View confirmEditText;
+	View loginButton;
+	View registerButton;
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		confirmTextView=findViewById(R.id.confirmTextView);
+		confirmEditText=findViewById(R.id.confirmEditText);
+		loginButton=findViewById(R.id.loginButton);
+		registerButton=findViewById(R.id.registerButton);
+		login(null);
+	}
+	public void login(View view)
+	{
+		getResources();
+		confirmTextView.setVisibility(View.GONE);
+		confirmEditText.setVisibility(View.GONE);
+		loginButton.setBackgroundColor(getResources().getColor(R.color.colorOrange));
+		registerButton.setBackgroundColor(getResources().getColor(R.color.colorYellow));
+	}
+	public void register(View view)
+	{
+		confirmTextView.setVisibility(View.VISIBLE);
+		confirmEditText.setVisibility(View.VISIBLE);
+		loginButton.setBackgroundColor(getResources().getColor(R.color.colorYellow));
+		registerButton.setBackgroundColor(getResources().getColor(R.color.colorOrange));
+	}
+	public void submit(View view)
+	{
+		startActivity(new Intent(this,BottomNavigationActivity.class));
+	}
+}
