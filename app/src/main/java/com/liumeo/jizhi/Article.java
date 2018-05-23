2a27f84efe27671e82cbddf7cb4e0f22b183b838
Liumeo <liumeo@pku.edu.cn>
2018-05-24 01:00:02 +0800

    自定义发现样式

---
+++ app/src/main/java/com/liumeo/jizhi/Article.java
@@ -0,0 +1,12 @@
+package com.liumeo.jizhi;
+
+public class Article
+{
+	String title;
+	String time;
+	Article(String title,String time)
+	{
+		this.title=title;
+		this.time=time;
+	}
+}
--- app/src/main/java/com/liumeo/jizhi/ItemFragment.java
+++ app/src/main/java/com/liumeo/jizhi/ItemFragment.java
@@ -5,15 +5,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemFragment extends ListFragment
 {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		ArrayAdapter<Object>adapter=new ArrayAdapter<Object>(inflater.getContext(),android.R.layout.simple_list_item_1,new Object[]{"2","3"});
+		ArrayList<Article>articles=new ArrayList<Article>();
+		articles.add(new Article("四六级报名开始","2018-03-11"));
+		articles.add(new Article("16级xxx有没有男/女朋友呀","2018-05-05"));
+		ListAdapter adapter=new ListViewAdapter<Article> (getContext(),articles,R.layout.article_item)
+		{
+			@Override
+			public void convert(ViewHolder holder, Article o)
+			{
+				((TextView)holder.getView(R.id.title)).setText(o.title);
+				((TextView)holder.getView(R.id.time)).setText(o.time);
+			}
+		};
 		setListAdapter(adapter);
 		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+		getListView().setDividerHeight(0);//取消边框线
 	}
 }
