a1b0e448d1a970a2582f9eab569f0e8c9fc35b6b
Liumeo <liumeo@pku.edu.cn>
2018-06-01 11:29:21 +0800

    消息源动态添加

---
+++ app/src/main/java/com/liumeo/jizhi/CheckBoxItem.java
@@ -0,0 +1,15 @@
+package com.liumeo.jizhi;
+
+import android.widget.CheckBox;
+
+class CheckBoxItem
+{
+	String text;
+	String srcID;
+	CheckBox checkBox;
+	CheckBoxItem(String text,String srcID)
+	{
+		this.text=text;
+		this.srcID=srcID;
+	}
+}
--- app/src/main/java/com/liumeo/jizhi/SettingActivity.java
+++ app/src/main/java/com/liumeo/jizhi/SettingActivity.java
@@ -1,22 +1,55 @@
 package com.liumeo.jizhi;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
-public class SettingActivity extends AppCompatActivity
+public class SettingActivity extends Activity
 {
-
+	CheckBoxItem[]checkBoxes;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setting);
+		LinearLayout sourceRelativeLayout=findViewById(R.id.sourceLinearLayout);
+		checkBoxes=new CheckBoxItem[]{new CheckBoxItem(getString(R.string.tree_hole),""),new CheckBoxItem(getString(R.string.dean),"")};
+		for(CheckBoxItem checkBoxItem:checkBoxes)
+		{
+			checkBoxItem.checkBox=new CheckBox(this);
+			checkBoxItem.checkBox.setText(checkBoxItem.text);
+			checkBoxItem.checkBox.setTag(checkBoxItem.srcID);
+			checkBoxItem.checkBox.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+			sourceRelativeLayout.addView(checkBoxItem.checkBox);
+		}
+		getSources();
+	}
+	private void getSources()
+	{
+		//request
+		new Networking.Updater()
+		{
+			@Override
+			public void run()
+			{
+				for(CheckBoxItem checkBoxItem:checkBoxes)
+				{
+					if(checkBoxItem.srcID=="")
+					{
+						checkBoxItem.checkBox.setChecked(true);
+					}
+				}
+			}
+		};
 	}
 	public void checkBoxClicked(View view)
 	{
+		String srcID=(String)view.getTag();
 		//消息源更新请求
 	}
 	public void logout(View view)
